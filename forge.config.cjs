28e7c9dd61d2d0054d9b1603b46ca7630a89cbdc
teddymarchildon <ted.marchildon@gmail.com>
2026-02-16 08:46:13 -0500

    Prepare for v 0.1

---
+++ forge.config.cjs
@@ -0,0 +1,62 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const { execSync } = require('child_process');
+const path = require('path');
+
+module.exports = {
+  outDir: 'forge-out',
+  packagerConfig: {
+    asar: true,
+    icon: './icon',
+    appBundleId: 'com.teddymarchildon.easycsv',
+    extendInfo: {
+      LSMinimumSystemVersion: '12.0',
+    },
+    osxSign: {
+      identity: 'Apple Distribution: Teddy Marchildon (55PJ732NTM)',
+      provisioningProfile: path.resolve(__dirname, 'EasyCSVPP.provisionprofile'),
+      entitlements: path.resolve(__dirname, 'entitlements.mas.plist'),
+      'entitlements-inherit': path.resolve(__dirname, 'entitlements.mas.child.plist'),
+    },
+    ignore: (filePath) => {
+      if (!filePath) return false;
+      if (
+        filePath.startsWith('/out') ||
+        filePath.startsWith('/node_modules') ||
+        filePath === '/package.json'
+      ) {
+        return false;
+      }
+      return true;
+    },
+  },
+  rebuildConfig: {},
+  hooks: {
+    generateAssets: async () => {
+      execSync('npx electron-vite build', { stdio: 'inherit' });
+    },
+  },
+  makers: [
+    {
+      name: '@electron-forge/maker-pkg',
+      config: {
+        identity: '3rd Party Mac Developer Installer: Teddy Marchildon (55PJ732NTM)',
+      },
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+};
